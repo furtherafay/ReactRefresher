b73573da35e1987fcd410065b8cb2d6d6830e7e9
Rafay Siddiqui <122159345+furtherafay@users.noreply.github.com>
2025-07-08 23:42:47 +0500

    feat: initial setup

--- reactbasics/src/App.jsx
+++ reactbasics/src/App.jsx
@@ -1,23 +1,11 @@
-import { use, useState, useEffect } from "react";
-import "./App.css";
-
-const Timer = () => {
-  const [secs, setSecs] = useState(0);
-
-  useEffect(()=>{
-    setInterval(() => {
-      setSecs((prev)=>prev+1)
-    }, 1000);
-    
-  }, [])
-
-  return (
-    <div><h2>{secs}</h2></div>
-  )
-};
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  return <Timer />;
+  return (
+    <>
+
+    </>
+  );
 };
 
 export default App;
